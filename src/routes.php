ec972e96bcae6c8f6adfb5df15f6fbea77731cb9
Igor-Rabodzei <igor.rabodzei@gmail.com>
2018-05-21 12:48:23 +0300

    RS-2739: was deleted endpoints getPatents and getSpaceSounds

--- src/routes.php
+++ src/routes.php
@@ -6,8 +6,6 @@ $routes = [
     'getAsteroidStats',
     'getAsteroids',
     'getEPICEarthImagery',
-    'getPatents',
-    'getSpaceSounds',
     'getEarthImagery',
     'getEarthAssets',
     'getMarsRoverPhotos',
